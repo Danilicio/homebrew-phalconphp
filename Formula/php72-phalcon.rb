23723d9103cc26350ef08d916f5863a402309c30
Danilicio <daeniee@gmail.com>
2018-07-05 14:39:08 +0700

    Update php72-phalcon.rb

--- Formula/php72-phalcon.rb
+++ Formula/php72-phalcon.rb
@@ -4,7 +4,7 @@ class Php72Phalcon < AbstractPhp72Extension
   init
   desc "Full-stack PHP framework"
   homepage "https://phalconphp.com/"
-  url "https://github.com/phalcon/cphalcon/archive/v3.4.0.zip"
+  url "https://github.com/phalcon/cphalcon/archive/v3.1.0.zip"
   sha256 "acbffa40a928672d1874345c2aa4eee6f05e087c943e155031db92db3cdc7429"
   head "https://github.com/phalcon/cphalcon.git"
 
