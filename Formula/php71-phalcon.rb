ac76eb6d0ff66211708f7bced753c849b4c7fa3f
Danilicio <daeniee@gmail.com>
2018-07-05 14:34:56 +0700

    Update php71-phalcon.rb

--- Formula/php71-phalcon.rb
+++ Formula/php71-phalcon.rb
@@ -5,7 +5,7 @@ class Php71Phalcon < AbstractPhp71Extension
   desc "Full-stack PHP framework"
   homepage "https://phalconphp.com/"
   url "https://github.com/phalcon/cphalcon/archive/v3.0.0.zip"
-  sha256 "acbffa40a928672d1874345c2aa4eee6f05e087c943e155031db92db3cdc7429"
+  sha256 "e5f69dd29f3ae05e267c7cbfd7d6b77ed541736eb3f2f14fe8124de3814d2f55"
   head "https://github.com/phalcon/cphalcon.git"
 
   depends_on "pcre"
